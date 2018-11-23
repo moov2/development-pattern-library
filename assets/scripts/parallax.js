ae5f389e67ed5df8c8d787e33284b2a18f57a9ef
mobinzk <mobinzk@gmail.com>
2018-11-23 12:43:19 +0000

    Re-structured the project
    - Added bundle.js
    - Added gallery
    - Moved parallex to it’s own js file
    - Added command to npm for mac and unix users

---
+++ assets/scripts/parallax.js
@@ -0,0 +1,41 @@
+/**
+ * Paralax
+ */
+
+import Rellax from 'rellax';
+
+const parallax = function ($el) {
+
+  // Handle single link
+  var rellax = new Rellax('.rellax', {
+    speed: -2,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
+
+  return {
+    rellax
+  }
+}
+
+const init = function () {
+  const selector = '.rellax'
+  let $elements = document.querySelectorAll(selector)
+  let instances = []
+  let instance
+
+  for (let i = 0; i < $elements.length; ++i) {
+    instance = parallax($elements[i])
+
+    if (instance) {
+      instances.push(instance)
+    }
+  }
+
+  return instances
+}
+
+init();
